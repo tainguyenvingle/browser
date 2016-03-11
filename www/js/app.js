c3759d366f80f125887862e89ab3d85c6d6ddd82
Vingle <vingle@Vingles-Mac-mini.local>
2016-03-11 10:53:55 +0700

    1.0.1

--- www/js/app.js
+++ www/js/app.js
@@ -24,74 +24,16 @@ angular.module('starter', ['ionic'])
   });
 })
 .controller('BrowserController',function($scope,$window,$http){
-  $scope.items=[
-                  {title:"Prism",link:'https://clay.io/game/prism'},
-                  {title:"Little Alchemy",link:'https://clay.io/game/littlealchemy'},
-                  {title:"Zepto Lab",link:'https://clay.io/game/ctr'},
-                  {title:"Turbo Squares",link:'http://play.bushidogames.com/en-gb//games/turbosquares/en-gb/#GameScreen'},
-                  {title:"Soko Toko",link:'http://play.bushidogames.com/en-gb//games/soko/en-gb/#mainMenu'},
-                  {title:"Steam Simon",link:'http://play.bushidogames.com/en-gb//games/steamsimon/en-gb/#%7B%22page%22%3A%22pageLoader%22%7D'},
-                  {title:"Space 4",link:'http://play.bushidogames.com/en-gb//games/space4inrow/en-gb/'},
-                  {title:"Crazy Gemz",link:'http://play.bushidogames.com/en-gb//games/gemsarcade2/en-gb/ '},
-                  {title:"Treasure of Sahara",link:'http://play.bushidogames.com/en-gb//games/treasuresofsahara/en-gb/'},
-                  {title:"Animory",link:'http://play.bushidogames.com/en-gb//games/animory/en-gb/'},
-                  {title:"Monster Keeper",link:'http://play.bushidogames.com/en-gb//games/monsterkeeper/en-gb/'},
-                  {title:"Groovy Ski",link:'http://m.blackmoondev.com/groovyski/'},
-                  {title:"Silver Arrow",link:'http://m.blackmoondev.com/silverarrow/'},
-                  {title:"Crunching Ninjas",link:'http://m.blackmoondev.com/cninjas/'},
-                  {title:"Penguin Blast",link:'http://m.blackmoondev.com/penguin/'},
-                  {title:"ZOO Pinball ",link:'http://m.blackmoondev.com/zoopinball/'},
-                  {title:"Troll Boxing",link:'http://m.blackmoondev.com/trollboxing/'}, 
-                  {title:"Flying School",link:'http://m.blackmoondev.com/flyingschool/'},
-                  {title:"Alien Attack",link:'http://m.blackmoondev.com/alienattack/'},
-                  {title:"Bakery Fun",link:'http://m.blackmoondev.com/bakeryfun/'},
-                  {title:"Full Immersion",link:'http://m.blackmoondev.com/fullimmersion/'},
-                  {title:"Pie Attack",link:'http://m.blackmoondev.com/pieattack/'},
-                  {title:"Triska",link:'http://m.blackmoondev.com/triska/'},
-                  {title:"Sheep",link:'http://m.blackmoondev.com/sheep/'},
-                  {title:"Hungry Fride",link:'http://m.blackmoondev.com/hungryfridge/'},
-                  {title:"Rec-Tang-Zings",link:'http://m.blackmoondev.com/rectangzings/'},
-                  {title:"Hag",link:'http://m.blackmoondev.com/hag/'},
-                  {title:"LBG",link:'http://m.blackmoondev.com/lbg/'},
-                  {title:"Rapunzel",link:'http://m.blackmoondev.com/rapunzel/'},
-                  {title:"Pigeon",link:'http://m.blackmoondev.com/pigeon/'},
-                  {title:"Mr.Bottomtight",link:'http://m.blackmoondev.com/bottomtight/'},
-                  {title:"Jellyland",link:'http://m.blackmoondev.com/jellyland/'},
-                  {title:"MonsterWC",link:'http://m.blackmoondev.com/monsterwc/'},
-                  {title:"Swipeart",link:'http://m.blackmoondev.com/swipeart/'},
-                  {title:"Space Cowboy",link:'http://m.blackmoondev.com/spacecowboy/'},
-                  {title:"Basketball Master",link:'http://m.blackmoondev.com/basketball/'},
-                  {title:"Tommy Monkey",link:'http://m.blackmoondev.com/tommy/'},
-                  {title:"Minigolf Kingdom",link:'http://m.blackmoondev.com/minigolf/'},
-                  {title:"Sally BBQ",link:'http://m.blackmoondev.com/sally/'},
-                  {title:"Sam Borcart Files",link:'http://m.blackmoondev.com/sam2/'},
-                  {title:"Goal! Goal! Goal!",link:'http://m.blackmoondev.com/goal/'},
-                  {title:"Let's Go Fishing",link:'http://m.blackmoondev.com/fishing/'},
-                  {title:"Donuts!",link:'http://m.blackmoondev.com/donuts/'},
-                  {title:"Don't Eat Trash",link:'http://m.blackmoondev.com/det/'},
-                  {title:"Greedy Gnomes",link:'http://m.blackmoondev.com/greedygnomes/'},
-                  {title:"Monsteroid",link:'http://m.blackmoondev.com/monsteroid/'},
-                  {title:"Captain Rogers : Incident at Rooku",link:'http://m.blackmoondev.com/crincident/'},
-                  {title:"Skater Dude",link:'http://m.blackmoondev.com/skaterdude/'},
-                  {title:"Evil Wyrm",link:'http://m.blackmoondev.com/wyrm/'},
-                  {title:"Saga of Craigen - The Stones of Thum",link:'http://m.blackmoondev.com/craigenSOT/'},
-                  {title:"Roder Rider",link:'http://m.blackmoondev.com/rodeo/'},
-                  {title:"Yummy Taco",link:'http://m.blackmoondev.com/taco/'},
-                  {title:"JomJom Jump",link:'http://m.blackmoondev.com/jomjom/'},
-                  {title:"Cinderella's Rush ",link:'http://m.blackmoondev.com/cinderella/'},
-                  {title:"Blackbeard's Island",link:'http://m.blackmoondev.com/blackbeard/'},
-                  {title:"Crazy Pizza",link:'http://m.blackmoondev.com/pizza/'},
-                  {title:"Fairy Princesses",link:'http://m.blackmoondev.com/fairy/'},
-                  {title:"Bike Tyke ",link:'http://m.blackmoondev.com/bike/'},
-                  {title:"Snack Time",link:'http://m.blackmoondev.com/snack/'},
-                  {title:"Cheese Lab",link:'http://m.blackmoondev.com/clab/'},
-                  {title:"Captain Rogers : Defense of Karmax-3",link:'http://m.blackmoondev.com/crkarmax/'},
-                  {title:"Pumpkin Smasher",link:'http://m.blackmoondev.com/pumpkin/'},
-                  {title:"Goblin Flying Machine",link:'http://m.blackmoondev.com/gfm/'},
-                ];
-
+  $scope.items=[];
+  $http.get('http://game.opl.kr/games_get.php').then(function(D) {
+    $scope.items=D.data.games;
+  });
+  // $scope.items=[
+  //                 {title:"Flappy Bird",link:'http://game.opl.kr/flappy-bird/'},
+  //                 {title:"Flappy 2048",link:'http://game.opl.kr/flappy-2048/'},
+  //               ];
   $scope.selectItem=function(index){
-    var ref = window.open($scope.items[index].link, '_blank', 'location=no,toolbar=no');
+    var ref = window.open('http://game.opl.kr/'+$scope.items[index].url, '_blank', 'location=no,toolbar=no');
     ref.addEventListener('loadstart', function(event) { 
           if(event.url=="http://close/")
             ref.close();
@@ -116,7 +58,7 @@ angular.module('starter', ['ionic'])
                       }
                   }
               );
-          },2500);
+          },3000);
     });
     ref.addEventListener('loaderror', function(event) { 
       ref.close();
@@ -158,7 +100,7 @@ angular.module('starter', ['ionic'])
                       }
                   }
               );
-          },2500);
+          },3000);
         });
         ref.addEventListener('loaderror', function(event) { 
           ref.close();
@@ -229,12 +171,20 @@ angular.module('starter', ['ionic'])
                       }
                   }
               );
-          },2000);
+          },3000);
     });
     ref.addEventListener('loaderror', function(event) { 
       ref.close();
       alert('Page not Found!');
     });
   }
+  //
+  // navigator.screenshot.save(function(error,res){
+  //     if(error){
+  //       console.error(error);
+  //     }else{
+  //       console.log('ok',res.filePath);
+  //     }
+  //   },'jpg',50);
 })
 

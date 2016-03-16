e11ebaa753b9ba251ac9cddad1de0610b8ec3244
Vingle <vingle@Vingles-Mac-mini.local>
2016-03-16 17:22:29 +0700

    1.0.3

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,9 @@
 function handleOpenURL(url) {
   var body = document.getElementsByTagName("body")[0];
   var mainController = angular.element(body).scope();
-  mainController.reportAppLaunched(url);
+  setTimeout(function() {
+      mainController.reportAppLaunched(url);
+  }, 0);
 }
 
 angular.module('starter', ['ionic'])
@@ -21,49 +23,74 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    ionic.Platform.fullScreen(true,true);
   });
 })
-.controller('BrowserController',function($scope,$window,$http){
+.controller('BrowserController',function($scope,$window,$http,$ionicActionSheet){
+  document.addEventListener("deviceready", function onDeviceReady(w) {
+    // Should work on Andriod
+    if(StatusBar && statusbarTransparent) {
+        // Enable translucent statusbar
+        statusbarTransparent.enable();
+
+        // Get the bar back
+        StatusBar.show();
+    }
+    // iOS only
+    else if (StatusBar) {
+        // Get the bar back
+        StatusBar.show();
+    }
+  }, false);
+  //get item
   $scope.items=[];
   $http.get('http://game.opl.kr/games_get.php').then(function(D) {
     $scope.items=D.data.games;
   });
-  // $scope.items=[
-  //                 {title:"Flappy Bird",link:'http://game.opl.kr/flappy-bird/'},
-  //                 {title:"Flappy 2048",link:'http://game.opl.kr/flappy-2048/'},
-  //               ];
+  $scope.currentGameIndex=0;
+  //iFrame
+  var containerFrame=document.getElementById('containerFrame');
+  var appFrame=document.getElementById('appFrame');
+  //button
+  var doneButton=document.getElementsByClassName('doneButton')[0];
+  var flag=0;
+  var x_pos = 0, y_pos = 0;
+  //event of doneButton
+  doneButton.style.top = (window.innerHeight-40)+'px';
+  doneButton.addEventListener("touchstart", function(){
+      flag = 0;
+  }, false);
+  doneButton.addEventListener("touchmove", function(e){
+      flag = 1;
+      x_pos = e.changedTouches[0].clientX;
+      y_pos = e.changedTouches[0].clientY;
+      if(x_pos<40)
+        this.style.left = '0px';
+      else if(x_pos>(window.innerWidth-40))
+        this.style.left = (window.innerWidth-40)+'px';
+      else
+        this.style.left = x_pos+'px';
+      if(y_pos<0)
+        this.style.top = '0px';
+      else if(y_pos>(window.innerHeight-40))
+        this.style.top = (window.innerHeight-40)+'px';
+      else
+        this.style.top = y_pos+ 'px';
+      e.preventDefault();
+  }, false);
+  doneButton.addEventListener("touchend", function(e){
+    if(flag==0){
+    }
+  }, false);
+  //--------
   $scope.selectItem=function(index){
-    var ref = window.open('http://game.opl.kr/'+$scope.items[index].url, '_blank', 'location=no,toolbar=no');
-    ref.addEventListener('loadstart', function(event) { 
-          if(event.url=="http://close/")
-            ref.close();
-    });
-    ref.addEventListener('loadstop', function(event) { 
-          ref.executeScript({ file: "http://mayblog.tk/openBrowser.js"});
-          ref.insertCSS({ file: "http://mayblog.tk/inAppStyle.css"});
-          //  get element for share
-          var loop = setInterval(function() {
-              ref.executeScript(
-                  {
-                      code: "document.getElementsByClassName('shareContent')[0].innerHTML"
-                  },
-                  function( values ) {
-                      console.log(values);
-                      if ( values==1) {
-                          ref.executeScript(
-                          {
-                              code: "document.getElementsByClassName('shareContent')[0].innerHTML=''"
-                          })
-                          $scope.shareFacebook(index);
-                      }
-                  }
-              );
-          },3000);
-    });
-    ref.addEventListener('loaderror', function(event) { 
-      ref.close();
-      alert('Page not Found!');
-    });
+    $scope.currentGameIndex=index;
+    containerFrame.style.display="block";
+    doneButton.style.display="block";
+    appFrame.setAttribute('src', 'http://game.opl.kr/'+$scope.items[index].url);
+    appFrame.addEventListener('loaderror',function(event){
+      alert('error');
+    })
   };
   //submit URL Form
   $scope.submitForm=function(){
@@ -75,37 +102,13 @@ angular.module('starter', ['ionic'])
       var pattern=/^[a-z0-9]*(\.[a-z0-9]*){1,2}/;
       var result=pattern.test(url);
       if(result){
-        var ref = window.open('http://'+url, '_blank', 'location=no,toolbar=no');
-        ref.addEventListener('loadstart', function(event) { 
-          if(event.url=="http://close/")
-            ref.close();
-        });
-        ref.addEventListener('loadstop', function(event) { 
-          ref.executeScript({ file: "http://mayblog.tk/openBrowser.js"});
-          ref.insertCSS({ file: "http://mayblog.tk/inAppStyle.css"});
-          var loop = setInterval(function() {
-              ref.executeScript(
-                  {
-                      code: "document.getElementsByClassName('shareContent')[0].innerHTML"
-                  },
-                  function( values ) {
-                      console.log(values);
-                      if ( values==1) {
-                          ref.executeScript(
-                          {
-                              code: "document.getElementsByClassName('shareContent')[0].innerHTML=''"
-                          })
-                          $scope.shareFacebook(0);
-                          
-                      }
-                  }
-              );
-          },3000);
-        });
-        ref.addEventListener('loaderror', function(event) { 
-          ref.close();
-          alert('Page not Found!');
-        });
+        cordova.plugins.Keyboard.close();
+        appFrame.setAttribute('src','http://'+url);
+        containerFrame.style.display="block";
+        doneButton.style.display="block";
+        appFrame.addEventListener('loaderror',function(event){
+          alert('error');
+        })
       }
       else{
         alert('Url format invalid!')
@@ -113,21 +116,21 @@ angular.module('starter', ['ionic'])
     }
   }
   //Share to FB
-  $scope.shareFacebook=function(index){
+  $scope.shareFacebook=function(id){
+    //create applink facebook
     $http({
       method:'POST',
       url:'https://graph.facebook.com/app/app_link_hosts',
       data:{
         access_token:"1583868701934656|6hjSnUvFY1dSZjsHk9zwihdv5dQ",
-        name:"iOS App Link Object Example",
-        ios:'[{"url" : "kioskbrowser://game/'+index+'","app_store_id" : 12345,"app_name" : "browser"},]',
-        android:'[{"url" : "kioskbrowser://game/'+index+'","package" : "com.ionicframework.browser105567","app_name" : "browser",},]',
+        name:"KioskBrowser AppLink",
+        ios:'[{"url" : "kioskbrowser://game/'+id+'","app_store_id" : 12345,"app_name" : "browser"},]',
+        android:'[{"url" : "kioskbrowser://game/'+id+'","package" : "com.ionicframework.browser105567","app_name" : "browser",},]',
         web:{
             "should_fallback" : false
             }
       }
     }).then(function(D){
-      console.log(D.data.id)
       var appLinkId=D.data.id;
       $http({
         method:'GET',
@@ -145,46 +148,57 @@ angular.module('starter', ['ionic'])
   //process params from AppLink FB
   $scope.reportAppLaunched = function(url) {
     var subString=/kioskbrowser\:\/\/game\/[0-9]+/i.exec(url);
-    var index = subString[0].substring(20);
-    var ref = window.open($scope.items[index].link, '_blank', 'location=no,toolbar=no');
-    ref.addEventListener('loadstart', function(event) { 
-          if(event.url=="http://close/")
-            ref.close();
-    });
-    ref.addEventListener('loadstop', function(event) { 
-          ref.executeScript({ file: "http://mayblog.tk/openBrowser.js"});
-          ref.insertCSS({ file: "http://mayblog.tk/inAppStyle.css"});
-          //  get element for share
-          var loop = setInterval(function() {
-              ref.executeScript(
-                  {
-                      code: "document.getElementsByClassName('shareContent')[0].innerHTML"
-                  },
-                  function( values ) {
-                      console.log(values);
-                      if ( values==1) {
-                          ref.executeScript(
-                          {
-                              code: "document.getElementsByClassName('shareContent')[0].innerHTML=''"
-                          })
-                          $scope.shareFacebook(index);
-                      }
-                  }
-              );
-          },3000);
-    });
-    ref.addEventListener('loaderror', function(event) { 
-      ref.close();
-      alert('Page not Found!');
-    });
+    var gameID = subString[0].substring(20);
+    if($scope._findGame(gameID)==true){
+      appFrame.setAttribute('src', 'http://game.opl.kr/'+$scope.items[$scope.currentGameIndex].url);
+      containerFrame.style.display="block";
+      doneButton.style.display="block";
+    }
   }
-  //
-  // navigator.screenshot.save(function(error,res){
-  //     if(error){
-  //       console.error(error);
-  //     }else{
-  //       console.log('ok',res.filePath);
-  //     }
-  //   },'jpg',50);
+  //actionSheet
+  $scope.show = function() {
+     var hideSheet = $ionicActionSheet.show({
+       buttons: [
+         { text: 'Share' },
+         { text: 'Exit' }
+       ],
+       cancelText: 'Cancel',
+       cancel: function() {
+            // add cancel code..
+          },
+       buttonClicked: function(index) {
+         buttonClicked(index);
+         return true;
+       }
+     });
+
+  };
+  //process actionSheet Button
+  function buttonClicked(index){
+    if(index==0){
+      $scope.shareFacebook($scope.items[$scope.currentGameIndex].id);
+    }else{
+      var r=confirm('Do you want close this window?');
+      if(r==true){
+        containerFrame.style.display="none";
+        doneButton.style.display="none";
+      };
+    }  
+  };
+  //findGame
+  $scope._findGame=function(id){
+    var flagExist=0;
+    for(var index=0;index<$scope.items.length;index++){
+      if(id==$scope.items[index].id){
+        $scope.currentGameIndex=index;
+        flagExist=1;
+        break;
+      }
+    }
+    if(flagExist==1)
+      return true;
+    else
+      return false;
+  } 
 })
 
